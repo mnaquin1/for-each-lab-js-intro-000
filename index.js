d83b99ac7129022b4e034b96080f776b3cfd59bf
Michael Naquin <mnaquin1@yahoo.com>
2020-09-10 20:14:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(array){
+  array.forEach((element, index,  array) =>{
+  console.log(`${index}: ${element}`)
+})
+}
+
+function iterate(callback){
+  var newArr = [1,2,3]
+  newArr.forEach(callback)
+    return newArr
+  )
+
+function doToArray(array, callback){
+  array.forEach(callback)
+}
